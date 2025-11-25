2781619f95b7587a069998aca7d4ab75be08f372
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-11-25 11:42:41 +0100

    Singleton Database connection

---
+++ src/app/core/Database.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Connection to database with PDO
+ */
+class Database{
+    /**
+     * Singelton to unique instance to database
+     * @var PDO
+     */
+    private static $pdo = null;
+
+    /**
+     * Function to create a new connection to database 
+     * @throws Exception if connection fails
+     * @return PDO|null 
+     */
+    public static function getConnection(){
+        if(self::$pdo === null){
+            $config = require __DIR__ . '/../../config/config.php';
+            $db = $config['db'];
+
+            $dsn = "mysql:host = {$db['host']};dbname={$db['dbname']};charset={$db['charset']}";
+
+            try{
+                self::$pdo = new PDO(
+                    $dns,
+                    $db['username'],
+                    $db['password'],
+                    [ // PDO options
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_EMULATE_PREPARES => false
+                    ]
+
+                    );
+            } catch(PDOException $e){
+                throw new Exception('Database connection error: ' . $e->getMessage());
+            }
+        return self::$pdo; // return the unique instance
+        
+        }
+    }
+}
