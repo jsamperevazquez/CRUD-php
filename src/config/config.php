e5b9cfd5ffb51b4a849c1c2a1ea30f3ecdef46d9
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-11-25 11:25:58 +0100

    config file

---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+?<?php 
+/**
+ * Configuration file for database connection
+ */
+
+return [
+    'db' => [
+        'host' => 'db',
+        'name' => 'gestion_proyectos',
+        'user' => 'user',
+        'pass' => 'userpass',
+        'charset' => 'utf8mb4'
+    ]
+    ];
